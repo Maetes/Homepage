720a1b260f07b862c5b81984072fdcd40060433e
TimTomczak <41620700+TimTomczak@users.noreply.github.com>
2021-09-09 15:51:43 +0200

    Satzende fehlte

--- resources/views/landingpage/section_services.blade.php
+++ resources/views/landingpage/section_services.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="flex-grow">
                     <h2 class="text-gray-900 text-lg font-title font-bold mb-3">Web- und App-Entwicklung</h2>
-                    <p class="leading-relaxed text-base font-basic flex-grow">Wir entwickeln die Website oder App, die Sie sich wünschen: Landingpages, Unternehmensportale, CMS-Systeme oder auch völlig individuelle Softwarelösungen. Unsere verwendeten Technologien finden Sie</p>
+                    <p class="leading-relaxed text-base font-basic flex-grow">Wir entwickeln die Website oder App, die Sie sich wünschen: Landingpages, Unternehmensportale, CMS-Systeme oder auch völlig individuelle Softwarelösungen. Einen Auszug unserer verwendeten Technologien finden Sie im Portfolio.</p>
                     <a class="mt-3 text-mtw-primary inline-flex items-center font-basic" href="#portfolio">Hier erfahren Sie mehr
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
