430fc2dc34700e3b873329671dc342bbfdc5c5e1
svnwa <svnwal@gmail.com>
2021-09-14 22:19:27 +0200

    Überflüssigen Font tag aus head entfernt

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -25,8 +25,6 @@
 <meta name="msapplication-TileImage" content="/img/logos/favicons/ms-icon-144x144.png">
 <meta name="theme-color" content="#005a80">
 
-<!-- Fonts -->
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 <script src="{{asset('/js/app.js')}}"></script>
 <script src="{{asset('/js/manifest.js')}}"></script>
