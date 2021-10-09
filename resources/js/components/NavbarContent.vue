dc0bde8f3e09bfe5de5340a1f4f42da76040a692
svnwa <svnwal@gmail.com>
2021-10-09 11:33:37 +0200

    problems resolved

--- resources/js/components/NavbarContent.vue
+++ resources/js/components/NavbarContent.vue
@@ -5,7 +5,7 @@
                 <img src="/img/logos/mtw.png" style="width: 100px;" alt="Logo">
             </a>
             <MenuComponent @disableNavbar="emitDisableNavbar" ref="menu"></MenuComponent>
-            <div  class="hidden h-full relative lg:flex w-3/5 xl:w-1/2 lg:flex w-1/2 justify-between items-center">
+            <div  class="hidden h-full relative xl:w-1/2 lg:flex w-1/2 justify-between items-center">
                 <a @click="toggleServicesDropdown" @mouseenter="showServicesDropdown"  class="relative inline-flex items-center" :class="{'border-b-4 border-mtw-secondary': windowHash==='#services'}">
                     <span>Services</span>
                     <img v-if="!servicesDropdownIsVisible" class="ml-1 w-4 h-4" src="/img/icons/chevron-down.svg">
@@ -14,7 +14,7 @@
                 <div @mouseleave="toggleServicesDropdown" v-if="servicesDropdownIsVisible" class="absolute mt-3 transform translate-y-20 -translate-x-12 ">
                     <div class="py-3 px-6 h-1/3 rounded-xl shadow-md font-basic text-sm bg-white border">
                         <div class="flex flex-col space-y-3">
-                            <a href="#services" class="ease-linear flex inline-flex items-center">
+                            <a href="#services" class="ease-linear inline-flex items-center">
                                 <span>Web- und Appentwicklung</span>
                             </a>
                             <a href="#services" class="">IT-Beratung</a>
@@ -28,7 +28,7 @@
                 <a class="relative inline-flex items-center" :class="{'border-b-4 border-mtw-secondary': windowHash==='#team'}" href="#team">Über uns</a>
                 <a class="relative inline-flex items-center" :class="{'border-b-4 border-mtw-secondary': windowHash==='#kontakt'}" href="#kontakt">Kontakt</a>
                 <div>
-                    <a href="/kundencenter" class="rounded-xl shadow-md px-4 py-2 flex inline-flex text-mtw-secondary items-center" :class="[showFixedNavbar ? 'bg-mtw-text ' : 'bg-mtw-primary ']">
+                    <a href="/kundencenter" class="rounded-xl shadow-md px-4 py-2 inline-flex text-mtw-secondary items-center" :class="[showFixedNavbar ? 'bg-mtw-text ' : 'bg-mtw-primary ']">
                         <span class="pr-2">Kundencenter</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
