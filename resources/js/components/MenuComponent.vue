55f9c688bdf156ca9a2fb13acc7661050618063f
svnwa <svnwal@gmail.com>
2021-10-11 12:44:04 +0200

    Menü angepasst

--- resources/js/components/MenuComponent.vue
+++ resources/js/components/MenuComponent.vue
@@ -29,15 +29,18 @@
                     </div>
                     <div  class="mx-auto px-5 sm:px-24 py-20">
                         <div class="flex flex-col items-center space-y-10 font-basic text-xl">
-                            <a @click="toggleMenu" class="transform hover:scale-105 " href="#services">Services</a>
-                            <a @click="toggleMenu" class="" href="#portfolio">Portfolio</a>
-                            <a @click="toggleMenu" class="" href="#team">Über uns</a>
-                            <a @click="toggleMenu" href="/kundencenter" class="rounded-xl shadow-md bg-mtw-primary px-4 py-2 flex inline-flex text-mtw-secondary items-center">
+                            <a @click="toggleMenu" class="transform hover:scale-105 " href="/#services">Services</a>
+                            <a @click="toggleMenu" class="" href="/#portfolio">Portfolio</a>
+                            <a @click="toggleMenu" class="" href="/#team">Über uns</a>
+                            <!-- <a @click="toggleMenu" href="/kundencenter" class="rounded-xl shadow-md bg-mtw-primary px-4 py-2 flex inline-flex text-mtw-secondary items-center">
                                 <span class="pr-2">Kundencenter</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
                                     <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                                 </svg>
+                            </a> -->
+                            <a @click="toggleMenu" href="/#kontakt" class="rounded-xl shadow-md bg-mtw-primary px-4 py-2 flex inline-flex text-mtw-secondary items-center">
+                                <span>Kontakt</span>
                             </a>
                         </div>
                     </div>
